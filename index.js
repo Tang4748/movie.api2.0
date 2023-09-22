7e15ef4c584c604da45be4932e17dd8cc33008ac
Ethan Bloom <109551346+Tang4748@users.noreply.github.com>
2023-09-22 03:21:23 -0500

    removed auth for /movies

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ app.get('/documentation', (req, res) => {
 // READ - returns all movies
 app.get(
   '/movies',
-  passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Movies.find()
       .then((movies) => res.status(200).json(movies))
